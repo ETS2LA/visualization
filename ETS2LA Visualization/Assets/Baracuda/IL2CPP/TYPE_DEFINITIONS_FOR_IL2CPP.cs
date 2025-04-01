6092c840d544abb75c0bc9d0bfdbc88ddd5bf8e5
Tumppi066 <83072683+Tumppi066@users.noreply.github.com>
2025-04-01 14:30:30 +0300

    Add loading bar when opening.

--- ETS2LA Visualization/Assets/Baracuda/IL2CPP/TYPE_DEFINITIONS_FOR_IL2CPP.cs
+++ ETS2LA Visualization/Assets/Baracuda/IL2CPP/TYPE_DEFINITIONS_FOR_IL2CPP.cs
@@ -5,7 +5,7 @@
 //---------- -------------------------- -------
 
 //Runtime Monitoring
-//File generated: 2025-03-25 16:54:49Z
+//File generated: 2025-04-01 14:27:29Z
 //Please dont change the contents of this file. Otherwise IL2CPP runtime may not work with runtime monitoring!
 //Ensure that this file is located in Assembly-CSharp. Otherwise this file may not compile.
 //If this file contains any errors please contact me and/or create an issue in the linked repository.
@@ -13,7 +13,7 @@
 
 #if ENABLE_IL2CPP && !DISABLE_MONITORING
 
-internal class IL2CPP_AOT_NGR0SHGJHL21PLRUPG05
+internal class IL2CPP_AOT_IX5U6HUPGAICEUV8WHAY
 {
     //Value Processor Method Definitions
 
